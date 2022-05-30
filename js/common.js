94c73570d674b6fbac4dce8501bb5fa998835d5a
psh827 <dhdp564@gmail.com>
2022-05-30 20:42:15 +0900

    footer-banner 완성

--- js/common.js
+++ js/common.js
@@ -2,7 +2,10 @@ const gnb_btn = document.querySelector(".gnb_btn");
 const all_menu_list = document.querySelector(".all-menu-list");
 const nav_li = document.querySelectorAll(".gnb-list_box > li");
 const notice_item = document.querySelectorAll(".notice-box .notice-item");
-const notice_content = document.querySelectorAll(".main-news-notice-wrap .notice-content");
+const notice_content = document.querySelectorAll(
+  ".main-news-notice-wrap .notice-content"
+);
+const select_box = document.querySelectorAll(".select-box");
 
 function addActive(item) {
   item.target.classList.toggle("active");
@@ -23,19 +26,30 @@ nav_li.forEach(function (item, index) {
     });
 });
 
-notice_item.forEach(function(item, index) {
-  item.addEventListener("click", function(){
-    notice_item.forEach(function(item){
+notice_item.forEach(function (item, index) {
+  item.addEventListener("click", function () {
+    notice_item.forEach(function (item) {
       item.classList.remove("active");
-    })
+    });
     this.classList.add("active");
-    notice_content.forEach(function(item){
+    notice_content.forEach(function (item) {
       item.classList.remove("active");
-    })
+    });
     notice_content[index].classList.add("active");
-  })
-})
+  });
+});
 
+select_box.forEach(function (item) {
+  item.addEventListener("click", function () {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    } else {
+      select_box.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+    }
+  });
+});
 
 gnb_btn.addEventListener("click", addActive);
-
