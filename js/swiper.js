a2bbcad3adb47f9281a4257cdbd42780fccc1812
psh827 <81910587+psh827@users.noreply.github.com>
2022-06-02 16:31:01 +0900

    finished

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,8 @@
 let totalNum = document.querySelectorAll(".main-area-slide .swiper-slide").length;
-
 const nowIndexText = document.querySelector(".now-index");
 const totalLengText = document.querySelector(".total-leng");
 totalLengText.innerText = totalNum;
+//mainbanner swiper
 var mySwiper = new Swiper(".swiper-container", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -17,11 +17,24 @@ var mySwiper = new Swiper(".swiper-container", {
   },
 });
 let nowIndex = 1;
+//main bannser pagenation custom
 nowIndexText.innerText = nowIndex;
 mySwiper.on("slideChange", function () {
   nowIndexText.innerText = this.realIndex + 1;
 });
 
+//agency swiepr 
+var agencySwiper = new Swiper(".swiper-container3", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  // loop: true,
+  navigation: {
+    nextEl: ".third-swiper-wrap .swiper-button-next.custom1",
+    prevEl: ".third-swiper-wrap .swiper-button-prev.custom1",
+  }
+ })
+
+//footerbanner
 var mysecondSwiper = new Swiper(".swiper-container2", {
   slidesPerView: 5,
   spaceBetween: 24,
@@ -33,10 +46,11 @@ var mysecondSwiper = new Swiper(".swiper-container2", {
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
-    reverseDirection: true,
+    reverseDirection: true, //마지막 slide 도착시 direction 반대전환
   }
 });
 
+//stop버튼 눌렀을 시 slide 정지, 재개 기능
 $(".swiper-auto-play").each(function(){
   $(this).click(function(){
     if ($(this).hasClass("pause")) {
